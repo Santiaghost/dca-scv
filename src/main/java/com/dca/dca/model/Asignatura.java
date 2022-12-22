faca08478544a24d678664a04ea7ad181b353d23
santisc santisc <santicallosino12@outlook.es>
2022-12-22 12:12:26 +0100

    Añadido modelo de asignatura

---
+++ src/main/java/com/dca/dca/model/Asignatura.java
@@ -0,0 +1,13 @@
+package com.dca.dca.model;
+
+public class Asignatura {
+    private int id;
+    private String nombre;
+    private int creditos;
+
+    public Asignatura(int id, String nombre, int creditos){
+        this.id=id;
+        this.nombre=nombre;
+        this.creditos=creditos;
+    }
+}
