1f25a69da508ed9720af411eb5457596230cd88f
santisc santisc <santicallosino12@outlook.es>
2022-12-22 11:32:52 +0100

    first commit

---
+++ src/main/java/com/dca/dca/DcaApplication.java
@@ -0,0 +1,13 @@
+package com.dca.dca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcaApplication.class, args);
+	}
+
+}
