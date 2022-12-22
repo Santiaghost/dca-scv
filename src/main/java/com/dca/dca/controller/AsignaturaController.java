47f8dbdaf4f37cf847d57bda5c9730ec2dd5cc34
santisc santisc <santicallosino12@outlook.es>
2022-12-22 12:23:09 +0100

    Error de compilacion

--- src/main/java/com/dca/dca/controller/AsignaturaController.java
+++ src/main/java/com/dca/dca/controller/AsignaturaController.java
@@ -1,5 +1,6 @@
 package com.dca.dca.controller;
 
+import com.dca.dca.model.Asignatura;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AsignaturaController {
     @GetMapping("/asignaturaDePrueba")
     public String muestraModeloDeAsignaturaDePrueba(){
-
-        return "prueba";
+        Asignatura asignatura = new Asignatura(1,"Desarrollo Colaborativo de Aplicaciones", 6);
+        return "Id:" + asignatura.getId() + " Nombre: " + asignatura.getNombre() + " Créditos: " + asignatura.getCreditos();
     }
 
 }
