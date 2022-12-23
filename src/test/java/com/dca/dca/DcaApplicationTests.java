4594289a6204ac9c23f712f10a7aa6c8f7dcb15a
santisc santisc <santicallosino12@outlook.es>
2022-12-23 12:54:53 +0100

    probando el hook para test

--- src/test/java/com/dca/dca/DcaApplicationTests.java
+++ src/test/java/com/dca/dca/DcaApplicationTests.java
@@ -1,13 +1,20 @@
 package com.dca.dca;
 
+import com.dca.dca.controller.AsignaturaController;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DcaApplicationTests {
 
+	@Autowired
+	private AsignaturaController controlador;
+
 	@Test
-	void contextLoads() {
+	void apiDevuelveBien() {
+		Assert.assertEquals(controlador.pruebaHook(),"Id:2 Nombre:MADS Creditos: 6 ");
 	}
 
 }
